e8cf5da29d1449cf4799261a0140662c0230cd13
Bojorquez Espinoza Jesus Antonio <bojorquez29an@gmail.com>
2022-09-09 11:48:58 -0600

    primera version de ng redit

---
+++ angular-reddit/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ArticleModel } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  article! : ArticleModel[];
+  constructor(){
+    this.article = [
+      new ArticleModel('Angular','http://angular.io',3),
+      new ArticleModel('Fullstack','http://Fullstack.io',2),
+      new ArticleModel('Angular Homepage','http://angular.io',1)
+    ];
+  }
+
+  addArticle(title:HTMLInputElement, link: HTMLInputElement): boolean { 
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+
+    return false; 
+  }
+ 
+}
